aacece8c8c3a956b53aec81e3291349787cc972f
Pranavjeet-Naidu <pranavjeetnaidu@gmail.com>
2025-12-23 23:58:48 +0530

    feat: notes+ mode mgmt
    Signed-off-by: Pranavjeet-Naidu <pranavjeetnaidu@gmail.com>

--- jel/jel.c
+++ jel/jel.c
@@ -1,19 +1,24 @@
+#include <stdlib.h>
 #include <unistd.h>
 #include <termios.h>
 
+struct termios orig_termios; //acts as the template struct to use (global variable)
+
+void disableRawMode(){
+  tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
+}
 void enableRawMode(){
-  struct termios raw;
-  tcgetattr(STDIN_FILENO, &raw); // reads current attributes into a struct
-  raw.c_lflag &= ~(ECHO); //ECHO to flush to terminal,disable it 
-                          //c_lflag = local flag basically
-  tcsetattr(STDIN_FILENO,TCSAFLUSH, &raw); //TCSAFLUSH->when to apply the attribute change
+  tcgetattr(STDIN_FILENO, &orig_termios);
+  atexit(disableRawMode);
+
+  struct termios raw = orig_termios; // use default struct again 
+  raw.c_lflag &= ~(ECHO | ICANON); 
+
+  tcsetattr(STDIN_FILENO,TCSAFLUSH, &raw); 
 }
 int main(){
   enableRawMode();
   char c;
-  // by default terminal is in cooked/canonical mode ( input sent only when user presses ENTER) , so use backspace to fix error
-  // raw mode is when each keypress is sent one by one 
-  // right now we are in canonical so we can make it so that pressing q and then ENTER quits the program 
-  while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q');
+    while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q');
   return 0;
 }
---
+++ jel/notes.md
@@ -0,0 +1,17 @@
+// by default terminal is in cooked/canonical mode ( input sent only when user presses ENTER) , so use backspace to fix error
+  // raw mode is when each keypress is sent one by one
+  // right now we are in canonical so we can make it so that pressing q and then ENTER quits the program
+
+- in order to disable canonical mode , we can also use the ICANON flag as a c_lflag
+
+- struct termios raw - basically a struct that is used to control the parameters of a terminal ( here , we call and refer it as raw )
+  mostly has these members:
+tcflag_t c_iflag: Input modes (e.g., handling of break conditions, parity errors, flow control).
+tcflag_t c_oflag: Output modes (e.g., post-processing, newline translation, delays).
+tcflag_t c_cflag: Control modes (e.g., character size, stop bits, modem control, hardware flow control).
+tcflag_t c_lflag: Local modes (e.g., enabling signals, canonical mode, character echoing).
+
+tcsetattr(filedes,when,termios struct) -> function to set the attributes
+we also have tcgetattr(filedes,termios struct)
+
+here we use TCSAFLUSH for when -> basically makes changes to the struct after all the output is written and then discards the input
