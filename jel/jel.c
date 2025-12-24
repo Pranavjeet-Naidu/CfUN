b66ceb56447298c2261db116cca81613d98d5317
Pranavjeet-Naidu <pranavjeetnaidu@gmail.com>
2025-12-24 15:21:36 +0530

    feat: add error handling
    Signed-off-by: Pranavjeet-Naidu <pranavjeetnaidu@gmail.com>

--- jel/jel.c
+++ jel/jel.c
@@ -1,4 +1,5 @@
 #include <ctype.h>
+#include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -6,31 +7,40 @@
 
 struct termios orig_termios; //acts as the template struct to use (global variable)
 
+//error handling , perror looks at 'errno' to get context
+void die(const char*s){
+  perror(s);
+  exit(1);
+}
 void disableRawMode(){
-  tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
+  if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios) == -1) die("tcsetattr");
 }
 void enableRawMode(){
-  tcgetattr(STDIN_FILENO, &orig_termios);
+  if (tcgetattr(STDIN_FILENO, &orig_termios) == -1) die("tcsetattr");
   atexit(disableRawMode);
 
   struct termios raw = orig_termios; // use default struct again 
   raw.c_lflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
   raw.c_oflag &= ~(OPOST);
   raw.c_cflag |= (CS8);
-  raw.c_lflag &= ~(ECHO | ICANON | ISIG | IEXTEN);  
+  raw.c_lflag &= ~(ECHO | ICANON | ISIG | IEXTEN);
+  raw.c_cc[VMIN] = 0;
+  raw.c_cc[VTIME] = 1;
 
-  tcsetattr(STDIN_FILENO,TCSAFLUSH, &raw); 
+  if (tcsetattr(STDIN_FILENO,TCSAFLUSH, &raw) == -1) die("tcsetattr"); 
 }
 int main(){
   enableRawMode();
 
-  char c;
-    while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q'){
+  while (1){
+    char c = '\0';
+    if (read(STDIN_FILENO,&c,1) == -1 && errno != EAGAIN) die("read");
     if (iscntrl(c)){
       printf("%d\r\n",c);
     }else{
       printf("%d ('%c')\r\n", c, c);
     }
+    if (c == 'q') break;
   }
 
   return 0;
