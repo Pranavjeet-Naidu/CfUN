f703ad7497972e303445ffbc876959ab82f29ea4
Pranavjeet-Naidu <pranavjeetnaidu@gmail.com>
2025-12-25 09:22:16 +0530

    feat: add windowsize for drawrows
    Signed-off-by: Pranavjeet-Naidu <pranavjeetnaidu@gmail.com>

--- jel/jel.c
+++ jel/jel.c
@@ -5,27 +5,39 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <termios.h>
+#include <sys/ioctl.h>
 
-
+/*** defines ***/
+#define CTRL_KEY(k) ((k) & 0x1f)
 /*** data ***/
+struct editorConfig{
+  int screenrows;
+  int screencols;
+  struct termios orig_termios; //acts as the template struct to use (global variable)
 
-struct termios orig_termios; //acts as the template struct to use (global variable)
+}
+struct editorConfig E;
 
 /*** terminal ***/
 
 //error handling , perror looks at 'errno' to get context
 void die(const char*s){
+  write(STDOUT_FILENO,"\x1b[2J",4); // to clear the screen on exit
+  write(STDOUT_FILENO,"\x1b[H",3);
+
   perror(s);
   exit(1);
 }
 void disableRawMode(){
-  if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios) == -1) die("tcsetattr");
+  if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &E.orig_termios) == -1) 
+    die("tcsetattr");
 }
 void enableRawMode(){
-  if (tcgetattr(STDIN_FILENO, &orig_termios) == -1) die("tcsetattr");
+  if (tcgetattr(STDIN_FILENO, &E.orig_termios) == -1) 
+    die("tcsetattr");
   atexit(disableRawMode);
 
-  struct termios raw = orig_termios; // use default struct again 
+  struct termios raw = E.orig_termios; // use default struct again 
   raw.c_lflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
   raw.c_oflag &= ~(OPOST);
   raw.c_cflag |= (CS8);
@@ -36,21 +48,65 @@ void enableRawMode(){
   if (tcsetattr(STDIN_FILENO,TCSAFLUSH, &raw) == -1) die("tcsetattr"); 
 }
 
+char editorReadKey() {
+  int nread;
+  char c;
+  while ((nread = read(STDIN_FILENO,&c,1)) != 1){
+    if (nread == -1 && errno != EAGAIN) die("read");
+  }
+  return c;
+}
+
+int getWindowSize(){
+  struct winsize ws;
+
+  if(ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1 || ws.ws_col == 0){
+    return -1;
+  } else{
+    *cols = ws.ws_col;
+    *rows = ws.ws_row;
+    return 0;
+  }
+}
+/*** output ***/
+
+void editorDrawRows(){
+  int y;
+  for(y=0;y<E.screenrows;y++){
+    write(STDOUT_FILENO,"%\r\n",3);
+  }
+}
+
+void editorRefreshScreen(){
+  write(STDOUT_FILENO,"\x1b[2J",4);//x1b [ J -> 27 followed by [ , then J to clear screen , 2 for entire screen , this is from VT100 escape sequences
+  write(STDOUT_FILENO,"\x1b[H",3); // the [ is used for escape chars, H is for positioning the cursor ,
+
+  editorDrawRows();
+  write(STDOUT_FILENO,"\x1b[H",3);
+}                                    
+
+/*** input ***/
+
+void editorProcessKeypress(){
+  char c = editorReadKey();
+
+  switch(c){
+    case CTRL_KEY('q'):
+      exit(0);
+      break;
+  }
+}
 
 /*** init ***/
 
+void initEditor(){
+  if (getWindowSize(&E.screenrows, &E.screencols) == -1) die("getWindowSize")
+}
 int main(){
   enableRawMode();
-
+  initEditor();
   while (1){
-    char c = '\0';
-    if (read(STDIN_FILENO,&c,1) == -1 && errno != EAGAIN) die("read");
-    if (iscntrl(c)){
-      printf("%d\r\n",c);
-    }else{
-      printf("%d ('%c')\r\n", c, c);
-    }
-    if (c == 'q') break;
+    editorProcessKeypress();
   }
 
   return 0;
