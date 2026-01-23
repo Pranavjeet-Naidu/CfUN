334ab8dd2547a7e2f8052b4a590725c8927c3787
Pranavjeet-Naidu <pranavjeetnaidu@gmail.com>
2026-01-23 09:39:12 +0530

    feat: color color
    Signed-off-by: Pranavjeet-Naidu <pranavjeetnaidu@gmail.com>

--- jel/jel.c
+++ jel/jel.c
@@ -41,12 +41,19 @@ enum editorKey{
   DEL_KEY
 };
 
+enum editorHighlight{
+  HL_NORMAL = 0,
+  HL_NUMBER,
+  HL_MATCH
+};
+
 /*** data ***/
 
 typedef struct erow{
   int size;
   int rsize;
   char *render;
+  unsigned char *hl;
   char *chars;
 } erow;
 
@@ -210,6 +217,29 @@ int getWindowSize(int *rows, int *cols){
   }
 }
 
+
+/*** syntax highlighting ***/
+
+void editorUpdateSyntax(erow *row){
+  row->hl = realloc(row->hl, row->rsize);
+  memset(row->hl, HL_NORMAL, row->rsize);
+
+  int i;
+  for (i = 0; i < row->rsize; i++){
+    if (isdigit(row->render[i])){
+      row->hl[i] = HL_NUMBER;
+    }
+  }
+}
+
+int editorSyntaxToColor(int hl){
+  switch(hl){
+    case HL_NUMBER: return 32;
+    case HL_MATCH: return 36;
+    default: return 37;
+  }
+}
+
 /*** row operations */
 
 void editorUpdateRow(erow *row){
@@ -233,6 +263,8 @@ void editorUpdateRow(erow *row){
   }
   row->render[idx] = '\0';
   row->rsize = idx;
+
+  editorUpdateSyntax(row);
   
 }
 
@@ -249,6 +281,7 @@ void editorInsertRow(int at,char *s, size_t len){
 
   E.row[at].rsize = 0;
   E.row[at].render = NULL;
+  E.row[at].hl = NULL;
   editorUpdateRow(&E.row[at]);
 
   E.numrows++;
@@ -304,6 +337,7 @@ void editorRowDelChar(erow *row, int at){
 void editorFreeRow(erow *row){
   free(row->render);
   free(row->chars);
+  free(row->hl);
 }
 
 void editorDelRow(int at){
@@ -483,6 +517,8 @@ void editorFindCallback(char *query, int key){
       E.cy = i;
       E.cx = editorRowRxToCx(row, match - row->render);
       E.rowoff = E.numrows;
+
+      memset(row->hl + (match - row->render), HL_MATCH, strlen(query));
       break;
     }
   }
@@ -576,16 +612,29 @@ void editorDrawRows(struct abuf *ab){
      if (len > E.screencols)
         len = E.screencols;
       char *c = &E.row[filerow].render[E.coloff];
+      unsigned char *hl = &E.row[filerow].hl[E.coloff];
+      int current_color = -1;
       int j;
       for (j=0;j<len;j++){
-        if(isdigit(c[j])){
-          abAppend(ab, "\x1b[32m",5);
-          abAppend(ab,&c[j],1);
-          abAppend(ab,"\x1b[39m",5);
-        } else{
-          abAppend(ab,&c[j],1);
+        if (hl[j] == HL_NORMAL){
+          if (current_color != -1){
+            abAppend(ab,"\x1b[39m",5);
+            current_color = -1;
+
         }
+          abAppend(ab,&c[j],1);
+        } else{
+          int color = editorSyntaxToColor(hl[j]);
+          if (color != current_color){
+            current_color = color;
+            char buf[16];
+            int clen = snprintf(buf,sizeof(buf),"\x1b[%dm",color);
+            abAppend(ab,buf,clen);
+          }
+          abAppend(ab,&c[j],1);
+        } 
       }
+      abAppend(ab,"\x1b[39m",5);
     }
 
     abAppend(ab, "\x1b[K",3);
